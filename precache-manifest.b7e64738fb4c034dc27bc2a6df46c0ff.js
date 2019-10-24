3fabc3076c90414787029858d0fa4fbd44d818f0
Rachael Yoder <wayoder4@gmail.com>
2019-10-24 19:05:12 -0400

    Updates

---
+++ precache-manifest.b7e64738fb4c034dc27bc2a6df46c0ff.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea8c364e8584e2eb616686c7a9fe4701",
+    "url": "/react-restaurant/index.html"
+  },
+  {
+    "revision": "548eea8d80e2460c2b49",
+    "url": "/react-restaurant/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "2284b7c1e8e428628ed6",
+    "url": "/react-restaurant/static/css/main.c534f773.chunk.css"
+  },
+  {
+    "revision": "548eea8d80e2460c2b49",
+    "url": "/react-restaurant/static/js/2.88451efc.chunk.js"
+  },
+  {
+    "revision": "2284b7c1e8e428628ed6",
+    "url": "/react-restaurant/static/js/main.bc475cde.chunk.js"
+  },
+  {
+    "revision": "2650c159c409280e9da5",
+    "url": "/react-restaurant/static/js/runtime-main.4681381b.js"
+  },
+  {
+    "revision": "78f4eac32b0511825b775aeea6944280",
+    "url": "/react-restaurant/static/media/accent.78f4eac3.png"
+  },
+  {
+    "revision": "4809f2cccad6a4195225288ab9386d28",
+    "url": "/react-restaurant/static/media/appetizer.4809f2cc.jpg"
+  },
+  {
+    "revision": "86bf0d5fcb9f6e498824852658722281",
+    "url": "/react-restaurant/static/media/breakfast.86bf0d5f.jpg"
+  },
+  {
+    "revision": "a60faf7b3808842a735fa0f9f47b590d",
+    "url": "/react-restaurant/static/media/chop.a60faf7b.jpg"
+  },
+  {
+    "revision": "65435c120b5374216432d013ecd07b1a",
+    "url": "/react-restaurant/static/media/dessert.65435c12.jpg"
+  },
+  {
+    "revision": "ffd6cb1843c4b449bae47721927c88a8",
+    "url": "/react-restaurant/static/media/dining.ffd6cb18.jpg"
+  },
+  {
+    "revision": "39c572cfb756ac114bd2b4140196b3d1",
+    "url": "/react-restaurant/static/media/lunch.39c572cf.jpg"
+  },
+  {
+    "revision": "6c7d67471d6b3fd07683f09343f7e247",
+    "url": "/react-restaurant/static/media/main-course.6c7d6747.jpg"
+  },
+  {
+    "revision": "478ba344e4018dce337cfd7fdad3b0b4",
+    "url": "/react-restaurant/static/media/sides.478ba344.jpg"
+  },
+  {
+    "revision": "f267bee71ceeffdd1b70e0631318d571",
+    "url": "/react-restaurant/static/media/wine.f267bee7.jpg"
+  }
+]);
